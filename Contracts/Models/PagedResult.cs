585d18c25331e12211ea2ff8dd6a1f8ce336a9f7
Bartłomiej Gromada <bartkuuu8167@gmail.com>
2023-03-11 15:08:01 +0100

    Fix for pagination

--- Contracts/Models/PagedResult.cs
+++ Contracts/Models/PagedResult.cs
@@ -3,13 +3,13 @@
 public sealed class PagedResult<T> where T : class
 {
     public List<T> Items { get; }
-    public bool HasNextPage { get; }
     public bool HasPreviousPage { get; }
+    public bool HasNextPage { get; }
 
     public PagedResult(List<T> items, int pageSize, int pageNumber)
     {
         Items = items;
-        HasPreviousPage = pageNumber > 0;
+        HasPreviousPage = pageNumber > 1;
         HasNextPage = items.Count >= pageSize;
     }
 }
